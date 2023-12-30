501799bcc52f58f7a1cfa03c52a6cb720be27304
999joaquin <joaquinwilbert166@gmail.com>
2023-12-31 02:04:48 +0700

    joaquin2

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use App\Models\User;
+use App\Models\Patient;
 
 
 class AdminController extends Controller
@@ -13,8 +15,30 @@ class AdminController extends Controller
         return view('admin.dashboard');
     }
 
-    public function viewPatients(){
+    public function viewPatients(){ // Joaquin
+        $patients = Patient::all();
         return view('admin.view-patients');
     }
 
+    public function editPatients(Patient $patient){ // Joaquin
+        return view('admin.edit-patients', compact('patient'));
+    }
+
+    public function updatePatients(Request $request, Patient $patient){ // Joaquin
+        $request->validate([
+            'date_of_birth' => 'required|date',
+            'phone' => 'required|string|max:12',
+            'address' => 'required|string'
+        ]);
+
+        $patient->update($request->all());
+
+        return redirect()->route('admin.patients.dashboard')->with('success', 'Data pasien telah berhasil di update');
+    
+    }
+
+    function destroyPatients(Patient $patient){ // Joaquin
+        $patient->delete();
+        return redirect()->route('admin.patients.dashboard')->with('success', 'Data pasien berhasil dihapus');
+    }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Patient;
 
 class UserController extends Controller
 {
@@ -11,7 +13,29 @@ class UserController extends Controller
         return view('user.dashboard');
     }
 
-    function showPatientsForm(){
-        return view('user.register-patient.blade.php');
+    function showRegistrationForm(){ // Joaquin
+        $user = auth()->user();
+        return view('user.register-patient', compact('user'));
+    }
+
+    function storeRegistration(Request $request){ // Joaquin
+        $request->validate([
+            'date_of_birth' => 'required|date',
+            'phone' => 'required|string|max:12',
+            'address' => 'required|string'
+        ]);
+
+        $user = auth()->user();
+
+        Patient::create([
+            'user_id' => $user->id,
+            'name' => $user->name,
+            'nim' => $user->nim,
+            'date_of_birth' => $request->date_of_birth,
+            'phone' => $request->phone,
+            'address' => $request->address
+        ]);
+
+        return redirect()->route('user.dashboard')->with('success', 'Patients telah berhasil ditambahkan');
     }
 }
