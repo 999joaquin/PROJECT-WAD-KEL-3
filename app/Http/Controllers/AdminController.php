497e8b0be9d104bec12a4e6d71b9716868704db8
stefanario <stefanario@student.telkomuniversity.ac.id>
2023-12-31 08:17:39 +0700

    membuat function addSchedule untuk controller admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
-
+use App\Models\Doctor;
+use App\Models\User;
+use App\Models\Schedule;
 
 class AdminController extends Controller
 {
@@ -13,8 +15,18 @@ class AdminController extends Controller
         return view('admin.dashboard');
     }
 
-    public function viewPatients(){
-        return view('admin.view-patients');
+    function addSchedule(Request $request){ // ario
+        $validateData = $request->validate([
+            'doctor_id' => 'required',
+            'day' => 'required',
+            'start_time' => 'required',
+            'end_time' => 'required'
+        ]);
+        
+        Schedule::create($validateData);
+
+        return redirect()->route('admin.viewSchedules')->with('success', 'Schedule added successfully.');
+        
     }
 
 }
